694178b76d144d4af14b218f5991f33010fe5199
Julian Grey <julianrgrey@gmail.com>
2015-02-09 12:44:49 +0000

    Enable on-the-fly changing of node's walkable property

--- Node.cs
+++ Node.cs
@@ -19,4 +19,35 @@ public class Node : MonoBehaviour {
 
         return (Mathf.Abs(x - targetX) + Mathf.Abs(y - targetY));
     }
+
+
+    void Update() {
+
+        if(Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit)) {
+                if(hit.collider.name == this.gameObject.name) {
+                    if(walkable == true) {
+                        walkable = false;
+                    }
+                    else {
+                        walkable = true;
+                    }
+                }
+            }
+        }
+        SetNodeColour();
+    }
+
+
+    void SetNodeColour() {
+
+        if(walkable == true) {
+            transform.Find("Sphere").renderer.material.color = Color.red;
+        }
+        else {
+            transform.Find("Sphere").renderer.material.color = Color.black;
+        }
+    }
 }
