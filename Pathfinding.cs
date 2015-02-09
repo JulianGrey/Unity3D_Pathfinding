ba129bca8ab932120f963d613fa2631198c3963e
Julian Grey <julianrgrey@gmail.com>
2015-02-09 18:49:57 +0000

    Added logic to backtrack down shortest path from the target back to the start

--- Pathfinding.cs
+++ Pathfinding.cs
@@ -4,17 +4,15 @@ using System.Collections.Generic;
 
 public class Pathfinding : MonoBehaviour {
 
-    public GameObject currentNode;
     public GameObject nextNode;
     public GameObject targetNode;
 
     public List<GameObject> nodeList = new List<GameObject>();
 
     public int distanceMoved = 0;
-    public int minNodeValue;
 
 
-	void Start() {
+    void Start() {
 
         nodeList = GameObject.Find("Level").GetComponent<BuildMap>().nodeList;
     }
@@ -23,8 +21,11 @@ public class Pathfinding : MonoBehaviour {
     public List<GameObject> Pathfinder(GameObject currentNode) {
 
         distanceMoved = 0;
-        minNodeValue = 0;
+
+        GameObject initialNode = currentNode;
+
         List<GameObject> closedList = new List<GameObject>();
+        List<GameObject> completeList = new List<GameObject>();
         List<GameObject> openList = new List<GameObject>();
 
         while(currentNode != targetNode) {
@@ -32,58 +33,98 @@ public class Pathfinding : MonoBehaviour {
                 closedList.Add(currentNode);
             }
 
-            List<GameObject> adjacentNodes = FindAdjacentNodes(currentNode, nodeList);
+            List<GameObject> adjacentNodes = FindAdjacentNodes(currentNode, nodeList, openList, closedList);
+            int minNodeValue = 0;
             nextNode = null;
             distanceMoved++;
 
             for (var i = 0; i < adjacentNodes.Count; i++) {
-                if(!closedList.Contains(adjacentNodes[i])) {
-                    if(!openList.Contains(adjacentNodes[i])) {
-                        int distanceFromTarget = adjacentNodes[i].GetComponent<Node>().FindDistanceToTarget(adjacentNodes[i].GetComponent<Node>().x, adjacentNodes[i].GetComponent<Node>().y, targetNode);
-                        adjacentNodes[i].GetComponent<Node>().parentNode = currentNode;
-                        adjacentNodes[i].GetComponent<Node>().distanceMoved = distanceMoved;
-                        adjacentNodes[i].GetComponent<Node>().distanceFromTarget = distanceFromTarget;
-                        adjacentNodes[i].GetComponent<Node>().moveCost = distanceMoved + distanceFromTarget;
-                        openList.Add(adjacentNodes[i]);
-                        if(nextNode == null) {
-                            nextNode = adjacentNodes[i];
-                            minNodeValue = adjacentNodes[i].GetComponent<Node>().moveCost;
-                        }
-                        else {
-                            if((adjacentNodes[i].GetComponent<Node>().moveCost) < minNodeValue) {
-                                nextNode = adjacentNodes[i];
-                                minNodeValue = adjacentNodes[i].GetComponent<Node>().moveCost;
-                            }
-                        }
-                    }
-                }
+                int distanceFromTarget = adjacentNodes[i].GetComponent<Node>().FindDistanceToTarget(adjacentNodes[i].GetComponent<Node>().x, adjacentNodes[i].GetComponent<Node>().y, targetNode);
+                adjacentNodes[i].GetComponent<Node>().parentNode = currentNode;
+                adjacentNodes[i].GetComponent<Node>().distanceMoved = distanceMoved;
+                adjacentNodes[i].GetComponent<Node>().distanceFromTarget = distanceFromTarget;
+                adjacentNodes[i].GetComponent<Node>().moveCost = distanceMoved + distanceFromTarget;
+                openList.Add(adjacentNodes[i]);
+                minNodeValue = GetLowestNodeValue(openList);
             }
+            nextNode = GetNextNode(openList, minNodeValue);
             closedList.Add(nextNode);
             openList.Remove(nextNode);
             currentNode = nextNode;
         }
-        return closedList;
+        completeList = FindCompletePath(closedList, initialNode);
+        completeList.Reverse();
+        return completeList;
     }
 
 
-    List<GameObject> FindAdjacentNodes(GameObject node, List<GameObject> nodeList) {
+    List<GameObject> FindAdjacentNodes(GameObject node, List<GameObject> nodeList, List<GameObject> openList, List<GameObject> closedList) {
 
         int currentNodeX = node.GetComponent<Node>().x;
         int currentNodeY = node.GetComponent<Node>().y;
 
         List<GameObject> nodes = new List<GameObject>();
-        if(nodeList.Find(foundNode => foundNode.GetComponent<Node>().x == currentNodeX && foundNode.GetComponent<Node>().y == currentNodeY + 1 && foundNode.GetComponent<Node>().walkable == true)) {
-            nodes.Add(nodeList.Find(foundNode => foundNode.GetComponent<Node>().x == currentNodeX && foundNode.GetComponent<Node>().y == currentNodeY + 1));
+
+        GameObject upNode = nodeList.Find(foundNode => foundNode.GetComponent<Node>().x == currentNodeX && foundNode.GetComponent<Node>().y == currentNodeY + 1 && foundNode.GetComponent<Node>().walkable == true);
+        GameObject rightNode = nodeList.Find(foundNode => foundNode.GetComponent<Node>().x == currentNodeX + 1 && foundNode.GetComponent<Node>().y == currentNodeY && foundNode.GetComponent<Node>().walkable == true);
+        GameObject downNode = nodeList.Find(foundNode => foundNode.GetComponent<Node>().x == currentNodeX && foundNode.GetComponent<Node>().y == currentNodeY - 1 && foundNode.GetComponent<Node>().walkable == true);
+        GameObject leftNode = nodeList.Find(foundNode => foundNode.GetComponent<Node>().x == currentNodeX - 1 && foundNode.GetComponent<Node>().y == currentNodeY && foundNode.GetComponent<Node>().walkable == true);
+
+        if(upNode != null && !openList.Contains(upNode) && !closedList.Contains(upNode)) {
+            nodes.Add(upNode);
         }
-        if(nodeList.Find(foundNode => foundNode.GetComponent<Node>().x == currentNodeX && foundNode.GetComponent<Node>().y == currentNodeY - 1 && foundNode.GetComponent<Node>().walkable == true)) {
-            nodes.Add(nodeList.Find(foundNode => foundNode.GetComponent<Node>().x == currentNodeX && foundNode.GetComponent<Node>().y == currentNodeY - 1));
+        if(rightNode != null && !openList.Contains(rightNode) && !closedList.Contains(rightNode)) {
+            nodes.Add(rightNode);
         }
-        if(nodeList.Find(foundNode => foundNode.GetComponent<Node>().x == currentNodeX + 1 && foundNode.GetComponent<Node>().y == currentNodeY && foundNode.GetComponent<Node>().walkable == true)) {
-            nodes.Add(nodeList.Find(foundNode => foundNode.GetComponent<Node>().x == currentNodeX + 1 && foundNode.GetComponent<Node>().y == currentNodeY));
+        if(downNode != null && !openList.Contains(downNode) && !closedList.Contains(downNode)) {
+            nodes.Add(downNode);
         }
-        if(nodeList.Find(foundNode => foundNode.GetComponent<Node>().x == currentNodeX - 1 && foundNode.GetComponent<Node>().y == currentNodeY && foundNode.GetComponent<Node>().walkable == true)) {
-            nodes.Add(nodeList.Find(foundNode => foundNode.GetComponent<Node>().x == currentNodeX - 1 && foundNode.GetComponent<Node>().y == currentNodeY));
+        if(leftNode != null && !openList.Contains(leftNode) && !closedList.Contains(leftNode)) {
+            nodes.Add(leftNode);
         }
         return nodes;
+    }
+
+
+    int GetLowestNodeValue(List<GameObject> openList) {
+
+        int minNodeValue = 0;
+        for(var i = 0; i < openList.Count; i++) {
+            if(minNodeValue == 0) {
+                minNodeValue = openList[i].GetComponent<Node>().moveCost;
+            }
+            else {
+                if(openList[i].GetComponent<Node>().moveCost < minNodeValue) {
+                    minNodeValue = openList[i].GetComponent<Node>().moveCost;
+                }
+            }
+        }
+        return minNodeValue;
+    }
+
+
+    GameObject GetNextNode(List<GameObject> openList, int minNodeValue) {
+
+        List<GameObject> nextNodeList = new List<GameObject>();
+        for(var i = 0; i < openList.Count; i++) {
+            if(openList[i].GetComponent<Node>().moveCost == minNodeValue) {
+                nextNodeList.Add(openList[i]);
+            }
+        }
+        return nextNodeList[nextNodeList.Count - 1];
+    }
+
+
+    List<GameObject> FindCompletePath(List<GameObject> closedList, GameObject start) {
+
+        List<GameObject> completeList = new List<GameObject>();
+        GameObject current = closedList.Find(node => node == targetNode);
+
+        completeList.Add(current);
+        while(current != start) {
+            completeList.Add(current.GetComponent<Node>().parentNode);
+            current = current.GetComponent<Node>().parentNode;
+        }
+        return completeList;
     }
 }
